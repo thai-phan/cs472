911fcd02acf90e1ba20a533e5f6c7a460b87d5e3
thaiphan <thaiphan2712@gmail.com>
2025-05-10 23:18:06 -0500

    update lab 08

--- lab8/prob1.js
+++ lab8/prob1.js
@@ -1,16 +1,22 @@
 class Meditation {
-    constructor(number) {
-        this.number = number;
-    }
+  constructor(duration) {
+    this.duration = duration;
+  }
 
-    start() {
-        console.log("Start meditation");
-        for (let i = this.number; i > 0; i--) {
-            console.log(i)
-        }
-        console.log("Jay Guru Dev");
+  start() {
+    console.log('Start meditation');
+    let count = this.duration;
+    const intervalId = setInterval(() => {
+      if (count > 0) {
+        console.log(count);
+        count--;
+      } else {
+        clearInterval(intervalId);
+        console.log('Jay Guru Dev');
+      }
+    }, 1000);
 
-    }
+  }
 }
 
 const morning_meditation = new Meditation(5);

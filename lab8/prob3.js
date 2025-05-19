aa98972a9e28d8ea572744b1d9b9f5a35e946052
thaiphan <thaiphan2712@gmail.com>
2025-05-18 21:20:21 -0500

    update

--- lab8/prob3.js
+++ lab8/prob3.js
@@ -1,11 +1,14 @@
-
-
 // Write an IIFE that fetch recipes from https://dummyjson.com/recipes and print to the console a list
 // of recipe names.
 
 
 fetch("https://dummyjson.com/recipes").then(res => res.json())
     .then(data => {
-        console.log(data.recipes.map(re => re.name))
-    })
+      console.log(data.recipes.map(re => re.name))
+    });
 
+(async () => {
+  const response = await fetch("https://dummyjson.com/recipes");
+  const data = await response.json();
+  console.log(data.recipes.map(re => re.name));
+})();
